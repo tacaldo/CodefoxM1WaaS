de6df1e74716eceed6caf8b29439e0647d59e8c1
codefoxtechnologies <tacaldo@gmail.com>
2021-03-10 16:57:51 -0500

    Work on nav component.

--- app/src/main/java/com/example/codefoxm1waas/StartNavFragment.kt
+++ app/src/main/java/com/example/codefoxm1waas/StartNavFragment.kt
@@ -1,10 +1,13 @@
 package com.example.codefoxm1waas
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,12 +32,33 @@ class StartNavFragment : Fragment() {
         }
     }
 
+    fun findJobsFrag() {
+        // Do something in response to button
+        Log.d("TAG", "Testing reached finfJobs in frag...")
+
+
+//        val intent = Intent(this, FindJobsActivity::class.java)
+//        startActivity(intent)
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start_nav, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_start_nav, container, false)
+
+        view.findViewById<Button>(R.id.bttnFindJobs).setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_startNavFragment_to_findJobsActivity)
+
+        )
+
+        //return inflater.inflate(R.layout.fragment_start_nav, container, false)
+
+        return view
+
     }
 
     companion object {
