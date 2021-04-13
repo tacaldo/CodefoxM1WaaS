692a58567a4c55448997777febfaafbb519a0163
codefoxtechnologies <tacaldo@gmail.com>
2021-04-13 19:01:33 -0400

    Work with Firebase..

--- app/src/main/java/com/codefoxflashworx/codefoxm1waas/MainActivity.kt
+++ app/src/main/java/com/codefoxflashworx/codefoxm1waas/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.codefoxflashworx.codefoxm1waas
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         // Configure sign-in to request the user's ID, email address, and basic
 // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
@@ -34,10 +38,51 @@ class MainActivity : AppCompatActivity() {
         //signInButton.setSize(SignInButton.SIZE_WIDE)
         signInButton.setSize(SignInButton.SIZE_WIDE)
 
+        //findViewById<SignInButton>(R.id.sign_in_button).onClick(  )
+
+        val signInIntent = mGoogleSignInClient.signInIntent
+//        startActivityForResult(signInIntent, RC_SIGN_IN)
+        startActivityForResult(signInIntent, 777)
+
+
 
 
 
     }
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == 777) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+//                Log.d("firebaseAuthWithGoogle:" + account.id)
+                Log.d("TAG", "firebaseAuthWithGoogle...$account.id")
+
+//                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+//                Log.w(TAG="", "Google sign in failed", e)
+                Log.d("TAG", "firebaseAuthWithGoogle...${e.message}")
+
+
+            }
+        }
+    }
+
+
+
+//    private fun signIn() {
+//        val signInIntent = mGoogleSignInClient.signInIntent
+//        startActivityForResult(signInIntent, RC_SIGN_IN)
+//    }
+
+
 
 
 }
